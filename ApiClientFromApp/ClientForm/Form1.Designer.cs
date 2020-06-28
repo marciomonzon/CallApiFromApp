f82015d84928b561fec4829dda27cb6a1f655761
Marcio <marcio.pcmonzon@gmail.com>
2020-06-27 22:36:36 -0300

    primeiro commit
    foi criado: camada com classes e models;
    camada de windows forms;
    camada de wpf.

---
+++ ApiClientFromApp/ClientForm/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(894, 278);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Call Async Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(530, 342);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(101, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(316, 342);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(101, 23);
+            this.btnPrev.TabIndex = 3;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 378);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+    }
+}
+
---
+++ ApiClientFromApp/ClientForm/Form1.cs
@@ -0,0 +1,82 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            btnNext.Enabled = false;
+            btnPrev.Enabled = false;
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+
+            currentNumber = comic.Num;
+
+            if (currentNumber > 0)
+            {
+                btnPrev.Enabled = true;
+            }
+
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            pictureBox1.Load(uriSource.AbsoluteUri);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await LoadImage();
+        }
+
+        private async void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                btnPrev.Enabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == maxNumber)
+                {
+                    btnNext.Enabled = false;
+                }
+            }
+        }
+
+        private async void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentNumber > 1)
+            {
+                currentNumber -= 1;
+                btnNext.Enabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == 1)
+                {
+                    btnPrev.Enabled = false;
+                }
+            }
+        }
+    }
+}
